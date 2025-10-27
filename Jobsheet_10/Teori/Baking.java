a1caa809cf1d603e9856ab9a6b42623ab8779258
Meutia Arafah Hidayat <meutiaarafah@gmail.com>
2025-10-27 08:51:55 +0700

    ini tugas teori pertemuan ke 10

---
+++ Jobsheet_10/Teori/Baking.java
@@ -0,0 +1,9 @@
+public abstract class Baking{
+    public String lamaProses;
+    public String suhuPenyimpanan;
+
+    public abstract void bahanBaku();
+    public void proses(){
+        System.out.println("Sedang proses baking produk");
+    };
+}
