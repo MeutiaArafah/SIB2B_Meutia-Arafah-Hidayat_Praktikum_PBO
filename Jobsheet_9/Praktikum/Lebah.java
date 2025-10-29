e89289eb623f5b24a1511fd169e2ed902a1d4cf2
Meutia Arafah Hidayat <meutiaarafah@gmail.com>
2025-10-29 23:59:13 +0700

    percobaan 1

---
+++ Jobsheet_9/Praktikum/Lebah.java
@@ -0,0 +1,10 @@
+package Jobsheet_9.Praktikum;
+
+public class Lebah extends Hewan {
+    public String kasta;
+
+    public Lebah(String kasta, double berat, String makanan, String habitat){
+        super(berat, makanan, habitat);
+        this.kasta = kasta;
+    }
+}
