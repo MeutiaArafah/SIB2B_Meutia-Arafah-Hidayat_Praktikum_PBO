395b8dd0480a5167175c4e97ee2959e612325a40
Meutia Arafah Hidayat <meutiaarafah@gmail.com>
2025-11-23 08:37:39 +0700

    Object Casting - Upcasting

--- Jobsheet_12/Praktikkum/Demo.java
+++ Jobsheet_12/Praktikkum/Demo.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class Demo {
     public static void main(String[] args) {
         
-        Dosen dosen1 = new Dosen("19940201", "Widia, S.Kom. M.Kom", "199402");
+        // Dosen dosen1 = new Dosen("19940201", "Widia, S.Kom. M.Kom", "199402");
         // Dosen dosen2 = new Dosen("19700105", "Muhammad, S.T, M.T", "197001");
         // TenagaKependidikan tendik1 = new TenagaKependidikan("19750301", "Aida, A.Md.", "Tenaga Administrasi");
         // TenagaKependidikan tendik2 = new TenagaKependidikan("19650304", "Rika, S.T.", "Tenaga Laboratorium");
@@ -15,12 +15,30 @@ public class Demo {
         // daftarPegawai.add(dosen2);
         // daftarPegawai.add(tendik1);
         // daftarPegawai.add(tendik2);
-    
+        
         // System.out.println("Jumlah Pegawai: " + daftarPegawai.size());
+        
+        // --- Object Casting ---
+        // Upcasting
+        // System.out.println(dosen1.nip);
+        // System.out.println(dosen1.nama);
+        // System.out.println(dosen1.nidn);
+        // dosen1.mengajar();
+        
+        // Dosen dosen1 = new Dosen("19940201", "Widia, S.Kom. M.Kom", "199402");
+        // System.out.println(pegawai1.nip);
+        // System.out.println(pegawai1.nama);
+        // System.out.println(pegawai1.nidn);
+        // pegawai1.mengajar();
+        
+        // // modifikasi 
+        Dosen dosen1 = new Dosen("19940201", "Widia, S.Kom. M.Kom", "199402");
+        Pegawai pegawai1 = dosen1; 
+        System.out.println(pegawai1.nip);
+        System.out.println(pegawai1.nama);
+        pegawai1.displayInfo();
 
-        System.out.println(dosen1.nip);
-        System.out.println(dosen1.nama);
-        System.out.println(dosen1.nidn);
-        dosen1.mengajar();
+        
+
     } 
 }
